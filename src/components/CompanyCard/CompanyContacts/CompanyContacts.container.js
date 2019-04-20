1cd403b181995865367f651302134990b57adaef
Víctor Oliva <olivarra1@gmail.com>
2019-04-20 15:02:38 +0200

    Solution using selector factories

--- src/components/CompanyCard/CompanyContacts/CompanyContacts.container.js
+++ src/components/CompanyCard/CompanyContacts/CompanyContacts.container.js
@@ -1,15 +1,13 @@
 import { connect } from 'react-redux';
 import { createSelector } from "reselect";
 
-import { getCompanyEmployees, getCompanyCustomers } from "../../../companies";
+import { makeGetCompanyEmployees, makeGetCompanyCustomers } from "../../../companies";
 import CompanyContacts from './CompanyContacts.component';
 
-const selectorCounts = createSelector(
-  [getCompanyCustomers, getCompanyEmployees],
+export default connect(() => createSelector(
+  [makeGetCompanyCustomers(), makeGetCompanyEmployees()],
   (customers, employees) => ({
     nCustomers: customers.length,
     nEmployees: employees.length
   })
-)
-
-export default connect(selectorCounts)(CompanyContacts);
+))(CompanyContacts);
